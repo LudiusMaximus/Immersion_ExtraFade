0fdde25917cf1b0a4ca81700d447ac030ebe0c05
LudiusMaximus <ludiusmaximus@gmail.com>
2019-08-13 15:20:44 +0200

    First commit.

---
+++ main.lua
@@ -0,0 +1,133 @@
+local folderName = ...
+local L = LibStub("AceAddon-3.0"):NewAddon(folderName, "AceTimer-3.0")
+
+
+-- Must fit the values of Immersion.
+local fadeOutTime = 0.2
+local fadeInTime = 0.5
+
+
+local StatusBarMod = Bartender4:GetModule("StatusTrackingBar")
+StatusBarMod:Enable()
+
+for _, frame in pairs(StatusBarMod.bar.manager.bars) do
+
+  local originalEnter = frame:GetScript("OnEnter")
+  local originalLeave = frame:GetScript("OnLeave")
+
+  frame:SetScript("OnEnter", function()
+    originalEnter(frame)
+    StatusBarMod.bar.manager.tempAlpha = StatusBarMod.bar.manager:GetAlpha()
+    StatusBarMod.bar.manager:SetAlpha(1)
+  end )
+
+  frame:SetScript("OnLeave", function()
+    originalLeave(frame)
+    if (StatusBarMod.bar.manager.tempAlpha ~= nil) then
+      StatusBarMod.bar.manager:SetAlpha(StatusBarMod.bar.manager.tempAlpha)
+    end
+  end )
+
+end
+
+
+
+
+local gossipShowFrame = CreateFrame("Frame")
+gossipShowFrame:RegisterEvent("GOSSIP_SHOW")
+gossipShowFrame:SetScript("OnEvent", function(self, event, ...)
+
+
+  ChatFrame1:SetIgnoreParentAlpha(true)
+  ChatFrame1Tab:SetIgnoreParentAlpha(true)
+  ChatFrame1EditBox:SetIgnoreParentAlpha(true)
+
+
+  StatusBarMod.bar.manager:SetIgnoreParentAlpha(true)
+
+  local StatusBarMod = Bartender4:GetModule("StatusTrackingBar")
+  -- Store tempAlpha for OnEnter/OnLeave.
+  StatusBarMod.bar.manager.tempAlpha = 0.25
+  UIFrameFadeOut(StatusBarMod.bar.manager, fadeOutTime, StatusBarMod.bar.manager:GetAlpha(), StatusBarMod.bar.manager.tempAlpha)
+
+
+  -- Hide frames of which we want no mouseover tooltips while faded.
+  L.frameHideTimer = LibStub("AceTimer-3.0"):ScheduleTimer(function()
+
+    if QuickJoinToastButton then QuickJoinToastButton:Hide() end
+    if PlayerFrame then PlayerFrame:Hide() end
+    if PetFrame then PetFrame:Hide() end
+    if TargetFrame then TargetFrame:Hide() end
+    if BuffFrame then BuffFrame:Hide() end
+    if DebuffFrame then DebuffFrame:Hide() end
+
+
+    if BT4Bar1 then BT4Bar1:Hide() end
+    if BT4Bar2 then BT4Bar2:Hide() end
+    if BT4Bar3 then BT4Bar3:Hide() end
+    if BT4Bar4 then BT4Bar4:Hide() end
+    if BT4Bar5 then BT4Bar5:Hide() end
+    if BT4Bar6 then BT4Bar6:Hide() end
+    if BT4Bar7 then BT4Bar7:Hide() end
+    if BT4Bar8 then BT4Bar8:Hide() end
+    if BT4Bar9 then BT4Bar9:Hide() end
+    if BT4Bar10 then BT4Bar10:Hide() end
+    if BT4BarBagBar then BT4BarBagBar:Hide() end
+    if BT4BarMicroMenu then BT4BarMicroMenu:Hide() end
+
+    if BT4BarStanceBar then BT4BarStanceBar:Hide() end
+    if BT4BarPetBar then BT4BarPetBar:Hide() end
+
+  end, fadeOutTime)
+
+end)
+
+
+
+local gossipClosedFrame = CreateFrame("Frame")
+gossipClosedFrame:RegisterEvent("GOSSIP_CLOSED")
+gossipClosedFrame:SetScript("OnEvent", function(self, event, ...)
+
+  -- Cancel hide timer if frames have not been hidden yet.
+  if L.frameHideTimer then
+    LibStub("AceTimer-3.0"):CancelTimer(L.frameHideTimer)
+  end
+
+  ChatFrame1:SetIgnoreParentAlpha(false)
+  ChatFrame1Tab:SetIgnoreParentAlpha(false)
+  ChatFrame1EditBox:SetIgnoreParentAlpha(false)
+
+  if QuickJoinToastButton then QuickJoinToastButton:Show() end
+  if PlayerFrame then PlayerFrame:Show() end
+  if PetFrame then PetFrame:Show() end
+  if TargetFrame then TargetFrame:Show() end
+  if BuffFrame then BuffFrame:Show() end
+  if DebuffFrame then DebuffFrame:Show() end
+
+
+
+  StatusBarMod.bar.manager:SetIgnoreParentAlpha(false)
+
+  if BT4Bar1 and BT4Bar1:GetAttribute("state-vis") ~= "hide" then BT4Bar1:Show() end
+  if BT4Bar2 and BT4Bar2:GetAttribute("state-vis") ~= "hide" then BT4Bar2:Show() end
+  if BT4Bar3 and BT4Bar3:GetAttribute("state-vis") ~= "hide" then BT4Bar3:Show() end
+  if BT4Bar4 and BT4Bar4:GetAttribute("state-vis") ~= "hide" then BT4Bar4:Show() end
+  if BT4Bar5 and BT4Bar5:GetAttribute("state-vis") ~= "hide" then BT4Bar5:Show() end
+  if BT4Bar6 and BT4Bar6:GetAttribute("state-vis") ~= "hide" then BT4Bar6:Show() end
+  if BT4Bar7 and BT4Bar7:GetAttribute("state-vis") ~= "hide" then BT4Bar7:Show() end
+  if BT4Bar8 and BT4Bar8:GetAttribute("state-vis") ~= "hide" then BT4Bar8:Show() end
+  if BT4Bar9 and BT4Bar9:GetAttribute("state-vis") ~= "hide" then BT4Bar9:Show() end
+  if BT4Bar10 and BT4Bar10:GetAttribute("state-vis") ~= "hide" then BT4Bar10:Show() end
+  if BT4BarBagBar and BT4BarBagBar:GetAttribute("state-vis") ~= "hide" then BT4BarBagBar:Show() end
+  if BT4BarMicroMenu and BT4BarMicroMenu:GetAttribute("state-vis") ~= "hide" then BT4BarMicroMenu:Show() end
+
+  if BT4BarStanceBar and BT4BarStanceBar:GetAttribute("state-vis") ~= "hide" then BT4BarStanceBar:Show() end
+  if BT4BarPetBar and BT4BarPetBar:GetAttribute("state-vis") ~= "hide" then BT4BarPetBar:Show() end
+
+
+  -- Fade in the only half faded status bar.
+  -- Store tempAlpha for OnEnter/OnLeave.
+  StatusBarMod.bar.manager.tempAlpha = 1
+  UIFrameFadeIn(StatusBarMod.bar.manager, fadeInTime, StatusBarMod.bar.manager:GetAlpha(), StatusBarMod.bar.manager.tempAlpha)
+
+end)
