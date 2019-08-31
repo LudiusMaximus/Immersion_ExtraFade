d64cabb64ed85453285d836c6fec3504b76e1b3e
LudiusMaximus <ludiusmaximus@gmail.com>
2019-08-31 15:05:10 +0200

    Added further events.

--- main.lua
+++ main.lua
@@ -38,7 +38,9 @@ local bagnonInventoryOpen = false
 
 local gossipShowFrame = CreateFrame("Frame")
 gossipShowFrame:RegisterEvent("GOSSIP_SHOW")
+gossipShowFrame:RegisterEvent("QUEST_COMPLETE")
 gossipShowFrame:RegisterEvent("QUEST_DETAIL")
+gossipShowFrame:RegisterEvent("QUEST_GREETING")
 gossipShowFrame:RegisterEvent("QUEST_PROGRESS")
 gossipShowFrame:SetScript("OnEvent", function(self, event, ...)
 
